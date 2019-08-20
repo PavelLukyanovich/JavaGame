62e61b786ad7a8bb699c6c16c8192c4c0e22175f
Pavel Lukyanovich <Pavellukyanovich1@gmail.com>
2019-08-20 23:30:05 +0300

    v1.2

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
+
 public class HiLo {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// Create the number for user to guess  
-		int theNumber = (int)(Math.random()*100 + 1);
-		System.out.println(theNumber);
-		int guess = 0;
-		System.out.println("Guess a number between 1 - 100:");
-		guess = scan.nextInt();
-		while(theNumber != guess) {
-			System.out.println("No! Try agane?");
-			continue;
-		}
-		if (theNumber == guess) {
-			System.out.println("You WIN!");
-		}
+		String playAgane = "";
+		do {
+			// Create the number for user to guess
+			int theNumber = (int) (Math.random() * 100 + 1);
+			System.out.println(theNumber);
+			int guess = 0;
 
+			while (theNumber != guess) {
+				System.out.println("Guess a number between 1 - 100:");
+				guess = scan.nextInt();
+				if (guess < 1 || guess > 100) {
+					System.out.println("Incorrect number");
+				}else if (guess < theNumber) {
+					System.out.println("More");
+					System.out.println("Try agane?");
+				} else if (guess > theNumber) {
+					System.out.println("Less");
+					System.out.println("No! Try agane?");
+				} else
+					System.out.println("You WIN!");
+			} // End of loop "while"
+			System.out.println("Would you like to play agane (y/n)?");
+			playAgane = scan.next();
+		} while (playAgane.equalsIgnoreCase("y")); // End of loop "do while96"
+		System.out.println("Thank you for good game! Goodbye!");
+		scan.close();
 	}
-
 }

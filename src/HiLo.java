a9562aa72176f891ffd36d1d25131e86007bea72
Pavel Lukyanovich <Pavellukyanovich1@gmail.com>
2019-08-20 23:48:24 +0300

    v1.3

--- src/HiLo.java
+++ src/HiLo.java
@@ -10,20 +10,27 @@ public class HiLo {
 			int theNumber = (int) (Math.random() * 100 + 1);
 			System.out.println(theNumber);
 			int guess = 0;
-
+			int count = 0;
 			while (theNumber != guess) {
 				System.out.println("Guess a number between 1 - 100:");
 				guess = scan.nextInt();
+				count++; // Move counter
 				if (guess < 1 || guess > 100) {
 					System.out.println("Incorrect number");
-				}else if (guess < theNumber) {
+				} else if (guess < theNumber) {
 					System.out.println("More");
 					System.out.println("Try agane?");
 				} else if (guess > theNumber) {
 					System.out.println("Less");
 					System.out.println("No! Try agane?");
-				} else
+				} else {
 					System.out.println("You WIN!");
+					if (count/1 == 1 ) {
+						System.out.println("You used " + count + " move");
+					}else
+					System.out.println("You used " + count + " moves");
+
+				}
 			} // End of loop "while"
 			System.out.println("Would you like to play agane (y/n)?");
 			playAgane = scan.next();
